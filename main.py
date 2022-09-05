f201b4c40de4eefc6e97bd88f0a632f304cc8b89
Pi <pimail@pi.com>
2022-09-05 16:54:27 +0200

    decrease refresh interval

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def rc_time(pin_to_circuit):
     # Output on the pin for
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
-    time.sleep(0.5)
+    time.sleep(0.1)
 
     # Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
