b5bec5b8e4a2169f445cf7361c38cc05c1867e9b
Kian Golawer <kian.golawer@posten.no>
2022-09-05 20:05:34 +0200

    cleanup

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from logging import handlers
 LOG_FILENAME = "log.out"
 formatter = logging.Formatter("%(asctime)s - %(message)s")
 
-handler = handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10*1024*1024, backupCount=2)
+handler = handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10 * 1024 * 1024, backupCount=2)
 handler.setFormatter(formatter)
 handler.setLevel(logging.DEBUG)
 
@@ -18,14 +18,13 @@ log = logging.getLogger('LOG')
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
 
-
 GPIO.setmode(GPIO.BOARD)
 
 # define the pin that goes to the circuit
-pin_to_circuit = 7
+ldr_pin = 7
 
 
-def rc_time(pin_to_circuit):
+def rc_time(pin_to_circuit=ldr_pin):
     count = 0
 
     # Output on the pin for
@@ -37,7 +36,7 @@ def rc_time(pin_to_circuit):
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
     # Count until the pin goes high
-    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+    while GPIO.input(pin_to_circuit) == GPIO.LOW:
         count += 1
         if count > 10000:
             return count
@@ -45,31 +44,31 @@ def rc_time(pin_to_circuit):
     return count
 
 
-# Catch when script is interrupted, cleanup correctly
 last_activated = datetime.now()
-c = 0
 
 try:
-    # Main loop
     while True:
         now = datetime.now()
-        v1, v2 = rc_time(pin_to_circuit), rc_time(pin_to_circuit)
-        val = (v1+v2)/2
+
+        v1, v2 = rc_time(), rc_time()
+        val = (v1 + v2) / 2
+        log.debug(f"V1:{v1} | V2:{v2} - avg:{val}")
+
         if v1 < 1200 and v2 < 1200:
             time_since_last_activation = (now - last_activated).seconds
             if time_since_last_activation < 10:
                 log.info("Skipping duplicate activation")
                 continue
+
             log.info(f"Activating switch {val} | (v1:{v1},v2:{v2})")
             hb.send_notification()
             last_activated = datetime.now()
             hb.activate_switch()
             time.sleep(1)
             hb.activate_switch()
-            if now.time().second % 5 == 0:
-                log.debug(f"V1:{v1} | V2:{v2} - avg:{val}")
+
 except KeyboardInterrupt as k:
-    log.error(k)
+    log.error("Error occurred", k)
     pass
 except Exception as e:
     log.error("Error occurred", e)
